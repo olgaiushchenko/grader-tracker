7e220bce1fc847ca889455e81f43d10c2ab60085
Olga Iushchenko <olga.yushchenko@gmail.com>
2018-06-05 14:22:05 -0700

    done

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+client = MongoClient('mongodb://localhost:27017')
+
+grades = client.tracker.grades
+
+# for _ in range(3):
+# 	name = input('student name:')
+# 	klass = input('student class:')
+# 	grade = float(input('grade:'))
+# 	d = {'name': name, 'klass':klass, 'grade':grade}
+# 	grades.insert_one(d)
+#	print(n
+total = 0
+count = grades.find().count()
+for g in grades.find():
+	total += g['grades']
+print(total/count) 
